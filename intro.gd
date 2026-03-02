84e79b5c99fab2a601a5ea022aedae021f65db36
Desync <chickenmanfy@proton.me>
2026-03-02 09:48:40 -0800

    fix critical bug breaking exports

--- intro.gd
+++ intro.gd
@@ -2,3 +2,7 @@ extends VideoStreamPlayer
 
 func _on_finished() -> void:
 	get_tree().change_scene_to_file("res://main.tscn")
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("skip cutscene"):
+		get_tree().change_scene_to_file("res://main.tscn")
